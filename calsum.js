6d00ee7f23ec2e828e011f7d99a57e04c1d823e5
ispandya <128423758+ispandya@users.noreply.github.com>
2024-02-26 17:42:54 -0500

    Add files via upload
    a js calculator that gives the sum when sent values in query (through get)

---
+++ calsum.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/sum", function(req,res){
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    const sum = a+b;
+    res.send(sum.toString());
+});
+app.listen(8080)
