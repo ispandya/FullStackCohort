25e7b26f53bb6f0a2537d3ee7f0121ef3dc9d519
ispandya <128423758+ispandya@users.noreply.github.com>
2024-03-06 18:08:49 -0500

    Add files via upload
    useMemo + useEffect

---
+++ useMemo(Eff).jsx
@@ -0,0 +1,31 @@
+// import React,{ Fragment } from 'react'
+import {useState} from "react"
+
+
+function App() {
+  const [counter, setCounter] = useState(0)
+  const [inputValue, setInputValue] = useState(1);
+  const [finalValue, setFinalValue] = useState(0);
+
+  useEffect(()=>{
+    let count =0;
+    for(let i=1; i<=inputValue; i++){
+      count = count + i;
+    }
+    setFinalValue(count);
+  }, [inputValue])
+
+  
+  return <div>
+    <input onChange={function(e){
+      setInputValue(e.target.value);
+    }}placeholedr={"Find sum from 1 ton n"}></input>
+    <br />
+    Sum from 1 to {inputValue} is {finalValue}
+    <br />
+    <button onClick={() =>{
+      setCounter(counter +1);
+    }}>Counter ({counter})</button>
+  </div>
+}
+export default App
