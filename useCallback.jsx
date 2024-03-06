ca22b9432c9f9735639e5bdd6a67d6e2442ba581
ispandya <128423758+ispandya@users.noreply.github.com>
2024-03-06 18:48:01 -0500

    Add files via upload
    useCallback

---
+++ useCallback.jsx
@@ -0,0 +1,23 @@
+// import React,{ Fragment } from 'react'
+import {useState, useEffect, useMemo} from "react"
+
+
+function App() {
+  const [count, setCount] = useState(0);
+  function logsomething(){
+    console.log("child clicked")
+  }
+  return <div>
+    <ButtonComponent inputFunction = {logsomething} />\
+    <button onClick={()=>{
+      setCount(count+1)
+    }}>Click me {count}</button>
+  </div>
+}
+const ButtonComponent = ({inputFunction})=>{
+  console.log("child render")
+  return <div>
+    <button onClick ={inputFunction}>Button clicked</button>
+  </div>
+}
+export default App
