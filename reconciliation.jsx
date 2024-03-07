f383eb6aeaa30cba6207c2afa5f96898dce6ba51
ispandya <128423758+ispandya@users.noreply.github.com>
2024-03-06 20:30:11 -0500

    Add files via upload
    Reconciliation

---
+++ reconciliation.jsx
@@ -0,0 +1,16 @@
+// import React,{ Fragment } from 'react'
+import {useState} from "react"
+
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <button onClick={function(){
+        setCount(count +1);
+      }}>Count is {count} </button>
+    </div>
+  )
+}
+
+export default App
