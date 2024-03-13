8a941cef6c5749ec72eb8dd6279af032e6fb7005
ispandya <128423758+ispandya@users.noreply.github.com>
2024-03-12 22:37:00 -0400

    Add files via upload
    Fetch vs Axios (Axios > Fetch)

---
+++ fthVaxs.js
@@ -0,0 +1,15 @@
+//axios vs fetch
+const axios = require("axios")
+
+//fetch
+async function main(){
+    const response = await fetch("https://sum-server.100xdevs.com/todos");
+    const json = await response.json();
+    console.log(json.todos.length);
+}
+
+//axios
+async function main(){
+    const response = await axios.get("https://sum-server.100xdevs.com/todos");
+    console.log(response.data.todos.length);
+}
