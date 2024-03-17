fc5232187d30ba3ddb775f532a69d6733bb82f78
ispandya <128423758+ispandya@users.noreply.github.com>
2024-03-17 01:28:48 -0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,7 +1,10 @@
 import React,{ Fragment } from 'react'
 import {useState} from "react"
 
-
+//state in React:
+//const arr = [1,2] , arr[0]=1 & arr[1]=2
+//const [a,b] = arr , a=1 && b=2
+//consr [a,b] = [1,2] , 1->a && 2->b
 function App() {
   const [title, setTitle] = useState("My name is ishita");
 
